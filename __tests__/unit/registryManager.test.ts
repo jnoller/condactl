9757c4f135ed18870301671ee6c4286fe1d87edd
Jesse Noller <jnoller@gmail.com>
2023-07-13 18:16:22 -0600

    Registry tests pass, make registry a bit more robust.

--- __tests__/unit/registryManager.test.ts
+++ __tests__/unit/registryManager.test.ts
@@ -1,110 +1,88 @@
 import fs from 'fs/promises';
 import { RegistryManager } from '../../src/registry';
 
-jest.mock('fs/promises');
+let tempDir: string;
+let registryManager: RegistryManager;
+const registryFileName = 'registry.json';
 
-describe('RegistryManager', () => {
-  let registryManager: RegistryManager;
+beforeEach(async () => {
+  tempDir = await fs.mkdtemp('/tmp/registry-test-');
+  registryManager = new RegistryManager(`${tempDir}/${registryFileName}`);
+  await registryManager.init();
+});
 
-  beforeEach(() => {
-    registryManager = new RegistryManager();
+afterEach(async () => {
+  try {
+    await fs.unlink(`${tempDir}/${registryFileName}`);
+  } catch (error) {
+    // Ignore error if file does not exist
+  }
+  await fs.rmdir(tempDir);
+});
+
+describe('isRegistryCreated', () => {
+  it('should return true if registry file exists', async () => {
+    jest.spyOn(fs, 'access').mockResolvedValueOnce();
+
+    const result = await registryManager.isRegistryCreated();
+
+    expect(fs.access).toHaveBeenCalledWith(registryManager['registryPath']);
+    expect(result).toBe(true);
   });
 
-  afterEach(() => {
-    jest.restoreAllMocks();
+  it('should return false if registry file does not exist', async () => {
+    jest.spyOn(fs, 'access').mockImplementationOnce(() => Promise.reject(new Error('File does not exist')));
+
+    const result = await registryManager.isRegistryCreated();
+
+    expect(fs.access).toHaveBeenCalledWith(registryManager['registryPath']);
+    expect(result).toBe(false);
+  });
+});
+
+describe('addEnvironment', () => {
+  it('should add environment to registry and save', async () => {
+    jest.spyOn(fs, 'writeFile').mockResolvedValueOnce();
+
+    await expect(registryManager.addEnvironment('environment1')).resolves.toBeUndefined();
+
+    expect(registryManager.getEnvironments()).toEqual({ environment1: true });
+    expect(fs.writeFile).toHaveBeenCalled();
   });
 
-  describe('loadRegistry', () => {
-    it('should load registry from file', async () => {
-      const registryData = JSON.stringify({ environment1: true, environment2: false });
-      jest.spyOn(fs, 'readFile').mockResolvedValue(registryData);
+  it('should handle error when adding environment', async () => {
+    jest.spyOn(fs, 'writeFile').mockImplementationOnce(() => Promise.reject(new Error('Failed to write file')));
 
-      await registryManager.loadRegistry();
+    await expect(registryManager.addEnvironment('environment1')).rejects.toThrow('Failed to write file');
+  });
+});
 
-      expect(fs.readFile).toHaveBeenCalledWith(registryManager.registryPath, 'utf-8');
-      expect(registryManager.getEnvironments()).toEqual({ environment1: true, environment2: false });
-    });
+describe('removeEnvironment', () => {
+  it('should remove environment from registry and save', async () => {
+    jest.spyOn(fs, 'writeFile').mockResolvedValueOnce();
+    await registryManager.addEnvironment('environment1');
 
-    it('should handle error when loading registry', async () => {
-      jest.spyOn(fs, 'readFile').mockRejectedValue(new Error('Failed to read file'));
+    await expect(registryManager.removeEnvironment('environment1')).resolves.toBeUndefined();
 
-      await registryManager.loadRegistry();
-
-      expect(fs.readFile).toHaveBeenCalledWith(registryManager.registryPath, 'utf-8');
-      expect(registryManager.getEnvironments()).toEqual({});
-    });
+    expect(registryManager.getEnvironments()).toEqual({});
+    expect(fs.writeFile).toHaveBeenCalled();
   });
 
-  describe('saveRegistry', () => {
-    it('should save registry to file', async () => {
-      const registryData = JSON.stringify({ environment1: true, environment2: false });
-      jest.spyOn(fs, 'writeFile').mockResolvedValue();
+  it('should handle error when removing environment', async () => {
+    await registryManager.addEnvironment('environment1');
+    jest.spyOn(fs, 'writeFile').mockImplementationOnce(() => Promise.reject(new Error('Failed to write file')));
 
-      await registryManager.saveRegistry();
-
-      expect(fs.writeFile).toHaveBeenCalledWith(registryManager.registryPath, registryData, 'utf-8');
-    });
-
-    it('should handle error when saving registry', async () => {
-      jest.spyOn(fs, 'writeFile').mockRejectedValue(new Error('Failed to write file'));
-
-      await expect(registryManager.saveRegistry()).rejects.toThrow('Failed to write file');
-    });
+    await expect(registryManager.removeEnvironment('environment1')).rejects.toThrow('Failed to write file');
   });
+});
 
-  describe('isRegistryCreated', () => {
-    it('should return true if registry file exists', async () => {
-      jest.spyOn(fs, 'access').mockResolvedValue();
+describe('getEnvironments', () => {
+  it('should return the environments from the registry', () => {
+    registryManager.addEnvironment('environment1');
+    registryManager.addEnvironment('environment2');
 
-      const result = await registryManager.isRegistryCreated();
+    const environments = registryManager.getEnvironments();
 
-      expect(fs.access).toHaveBeenCalledWith(registryManager.registryPath);
-      expect(result).toBe(true);
-    });
-
-    it('should return false if registry file does not exist', async () => {
-      jest.spyOn(fs, 'access').mockRejectedValue(new Error('File not found'));
-
-      const result = await registryManager.isRegistryCreated();
-
-      expect(fs.access).toHaveBeenCalledWith(registryManager.registryPath);
-      expect(result).toBe(false);
-    });
-  });
-
-  describe('addEnvironment', () => {
-    it('should add environment to registry and save', async () => {
-      jest.spyOn(fs, 'writeFile').mockResolvedValue();
-
-      await registryManager.addEnvironment('environment1');
-
-      expect(registryManager.getEnvironments()).toEqual({ environment1: true });
-      expect(fs.writeFile).toHaveBeenCalled();
-    });
-
-    it('should handle error when adding environment', async () => {
-      jest.spyOn(fs, 'writeFile').mockRejectedValue(new Error('Failed to write file'));
-
-      await expect(registryManager.addEnvironment('environment1')).rejects.toThrow('Failed to write file');
-    });
-  });
-
-  describe('removeEnvironment', () => {
-    it('should remove environment from registry and save', async () => {
-      jest.spyOn(fs, 'writeFile').mockResolvedValue();
-      registryManager.addEnvironment('environment1');
-
-      await registryManager.removeEnvironment('environment1');
-
-      expect(registryManager.getEnvironments()).toEqual({});
-      expect(fs.writeFile).toHaveBeenCalled();
-    });
-
-    it('should handle error when removing environment', async () => {
-      jest.spyOn(fs, 'writeFile').mockRejectedValue(new Error('Failed to write file'));
-      registryManager.addEnvironment('environment1');
-
-      await expect(registryManager.removeEnvironment('environment1')).rejects.toThrow('Failed to write file');
-    });
+    expect(environments).toEqual({ environment1: true, environment2: true });
   });
 });
--- src/registry.ts
+++ src/registry.ts
@@ -2,16 +2,28 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export class RegistryManager {
-  private registryPath: string;
+  public registryPath: string;
   private registry: { [key: string]: boolean };
 
-  constructor() {
-    this.registryPath = path.join(process.env.HOME ?? '', '.cope', 'registry.json');
+  constructor(registryPath?: string) {
+    this.registryPath = registryPath ?? path.join(process.env.HOME ?? '', '.cope', 'registry.json');
     this.registry = {};
-    this.loadRegistry();
   }
 
-  public async loadRegistry(): Promise<void> {
+  public async init(): Promise<void> {
+    try {
+      await fs.access(this.registryPath);
+    } catch (error) {
+      if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
+        await this.saveRegistry(); // This will create the file with the initial state of the registry
+      } else {
+        throw error; // If the error is not 'ENOENT', rethrow it
+      }
+    }
+    await this.loadRegistry();
+  }
+
+  private async loadRegistry(): Promise<void> {
     try {
       const registryData = await fs.readFile(this.registryPath, 'utf-8');
       this.registry = JSON.parse(registryData);
@@ -20,9 +32,14 @@ export class RegistryManager {
     }
   }
 
-  public async saveRegistry(): Promise<void> {
-    const registryData = JSON.stringify(this.registry, null, 2);
-    await fs.writeFile(this.registryPath, registryData, 'utf-8');
+  private async saveRegistry(): Promise<void> {
+    try {
+      const registryData = JSON.stringify(this.registry, null, 2);
+      await fs.writeFile(this.registryPath, registryData, 'utf-8');
+    } catch (error) {
+      console.error('Failed to write file:', error);
+      throw error; // Re-throw the error after logging it
+    }
   }
 
   public async isRegistryCreated(): Promise<boolean> {
