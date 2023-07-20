23be15a8f6c5ba91c3a06ae1ea2773e5ab5bc0cb
Jesse Noller <jnoller@gmail.com>
2023-07-20 10:38:33 -0600

    CLI works, use yarn build, yarn link

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// Import the EnvironmentManager class from the environmentManager module
+import { EnvironmentManager } from './environmentManager';
+import './cli';
+
+// Export the EnvironmentManager class as a named export
+export { EnvironmentManager };
+
