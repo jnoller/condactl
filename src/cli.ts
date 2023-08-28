0842cd69ea6dd7c501e5d307509f2ddee997b08d
Jesse Noller <jnoller@gmail.com>
2023-08-28 11:04:21 -0600

    checkpoint cli refactor

--- src/cli.ts
+++ src/cli.ts
@@ -2,142 +2,164 @@
 
 import { Command } from 'commander';
 import { EnvironmentManager } from './environmentManager';
+import createLogger from './logging';
+import { LOG_LEVEL } from './config';
 
 const program = new Command();
-const environmentManager = EnvironmentManager.getInstance();
+const logger = createLogger(LOG_LEVEL);
+const manager = EnvironmentManager.getInstance();
+
 
 program
-  .version('0.0.1')
-  .description('CLI for managing Conda environments');
-
-program
-  .command('discover')
-  .description('Discover available environments')
-  .action(async () => {
-    console.log('Discovering environments...');
-    try {
-      const environments = await environmentManager.discoverEnvironments(true);
-      console.log('Discovered environments:', environments);
-    } catch (error) {
-      console.error('Failed to discover environments:', error);
-    }
-  });
-
-program
-  .command('clean')
-  .description('Clean all environments')
-  .action(async () => {
-    console.log('Cleaning all environments...');
-    try {
-      await environmentManager.cleanEnvironment();
-      console.log('All environments cleaned.');
-    } catch (error) {
-      console.error('Failed to clean environments:', error);
-    }
-  });
-
-program
-  .command('create <name> [path]')
-  .description('Create a new environment')
-  .action(async (name, path) => {
-    console.log(`Creating environment ${name}...`);
-    try {
-      await environmentManager.createEnvironment(name, path);
-      console.log(`Environment ${name} created.`);
-    } catch (error) {
-      console.error(`Failed to create environment ${name}:`, error);
-    }
-  });
-
-program
-  .command('remove <nameOrPath>')
-  .description('Remove an environment')
-  .action(async (nameOrPath) => {
-    console.log(`Removing environment ${nameOrPath}...`);
-    try {
-      await environmentManager.removeEnvironment(nameOrPath);
-      console.log(`Environment ${nameOrPath} removed.`);
-    } catch (error) {
-      console.error(`Failed to remove environment ${nameOrPath}:`, error);
-    }
-  });
-
-program
-  .command('install <environment> <packageOrFile>')
-  .description('Install a package in an environment')
-  .action(async (environment, packageOrFile) => {
-    console.log(`Installing package ${packageOrFile} in environment ${environment}...`);
-    try {
-      await environmentManager.installPackage(environment, packageOrFile);
-      console.log(`Package ${packageOrFile} installed in environment ${environment}.`);
-    } catch (error) {
-      console.error(`Failed to install package ${packageOrFile} in environment ${environment}:`, error);
-    }
-  });
-
-program
-  .command('uninstall <environment> <packageName>')
-  .description('Uninstall a package from an environment')
-  .action(async (environment, packageName) => {
-    console.log(`Uninstalling package ${packageName} from environment ${environment}...`);
-    try {
-      await environmentManager.uninstallPackage(environment, packageName);
-      console.log(`Package ${packageName} uninstalled from environment ${environment}.`);
-    } catch (error) {
-      console.error(`Failed to uninstall package ${packageName} from environment ${environment}:`, error);
-    }
-  });
-
-program
-  .command('list-packages <name>')
-  .description('List all packages in an environment')
+  .command('create <name>')
+  .description('create a new environment')
   .action(async (name) => {
-    console.log(`Listing packages in environment ${name}...`);
     try {
-      const packages = await environmentManager.listPackages(name);
-      console.log(`Packages in environment ${name}:`, packages);
+      const environment = await manager.createEnvironment(name);
+      logger.info(`Created environment: ${JSON.stringify(environment)}`);
     } catch (error) {
-      console.error(`Failed to list packages in environment ${name}:`, error);
+      logger.error(`Failed to create environment: ${error}`);
+    }
+  });
+
+
+program
+  .command('discoverEnvironments')
+  .option('-f, --force', 'force refresh')
+  .action(async (options) => {
+    const result = await manager.discoverEnvironments(options.force);
+    logger.info(`Environments discovery was successful: ${result}`);
+  });
+
+program
+  .command('renameEnvironment')
+  .argument('<oldName>', 'name of the environment to rename')
+  .argument('<newName>', 'new name for the environment')
+  .option('[dryRun]', 'perform a dry run', false)
+  .description('rename an existing environment')
+  .action(async (oldName, newName, options) => {
+    const dryRun = options['dryRun'] || false;
+    try {
+      const newEnvironment = await manager.renameEnvironment(oldName, newName, dryRun);
+      logger.info(`Renamed environment: ${JSON.stringify(newEnvironment)}`);
+    } catch (error) {
+      logger.error(`Failed to rename environment: ${error}`);
+    }
+  });
+
+  program
+  .command('removeEnvironment <name>')
+  .option('--json', 'Output as JSON')
+  .action(async (name, cmd) => {
+    try {
+      const result = await manager.removeEnvironment(name, cmd.json);
+      logger.info(`Removed environment: ${JSON.stringify(result)}`);
+    } catch (error) {
+      logger.error(`Failed to remove environment: ${error}`);
     }
   });
 
 program
-  .command('search <name>')
-  .description('Search for a package')
-  .action(async (name) => {
-    console.log(`Searching for package ${name}...`);
-    try {
-      const result = await environmentManager.searchForPackage(name);
-      console.log(`Search result for package ${name}:`, result);
-    } catch (error) {
-      console.error(`Failed to search for package ${name}:`, error);
-    }
+  .command('listEnvironments')
+  .option('-f, --forceRefresh', 'Force Refresh', false)
+  .option('--json', 'Output as JSON')
+  .action(async (cmd) => {
+    const environments = await manager.listEnvironments(cmd.forceRefresh, cmd.json);
+    logger.info(`List of environments: ${JSON.stringify(environments)}`);
   });
 
 program
-  .command('version')
-  .description('Get Conda version')
-  .action(async () => {
-    console.log('Getting Conda version...');
-    try {
-      const version = await environmentManager.getCondaVersion();
-      console.log('Conda version:', version);
-    } catch (error) {
-      console.error('Failed to get Conda version:', error);
-    }
+  .command('listPackages <environment>')
+  .option('-f, --forceRefresh', 'Force Refresh', false)
+  .option('--json', 'Output as JSON')
+  .action(async (environment, cmd) => {
+    const packages = await manager.listPackages(environment, cmd.json, cmd.forceRefresh);
+    logger.info(`List of packages in environment: ${JSON.stringify(packages)}`);
   });
 
 program
-  .command('run <environment> <command>')
-  .description('Run a command in a Conda environment')
+  .command('updateEnvironmentPackages <name>')
+  .option('--json', 'Output as JSON')
+  .action(async (name, cmd) => {
+    const packages = await manager.updateEnvironmentPackages(name, cmd.json);
+    logger.info(`Updated environment packages: ${JSON.stringify(packages)}`);
+  });
+
+program
+  .command('installPackage <environment> <name>')
+  .option('-v, --version [version]', 'Package Version')
+  .option('--json', 'Output as JSON')
+  .action(async (environment, name, cmd) => {
+    const result = await manager.installPackage(environment, name, cmd.json, cmd.version);
+    logger.info(`Installed package: ${JSON.stringify(result)}`);
+  });
+
+program
+  .command('uninstallPackage <environment> <name>')
+  .option('--json', 'Output as JSON')
+  .action(async (environment, name, cmd) => {
+    const result = await manager.uninstallPackage(environment, name, cmd.json);
+    logger.info(`Removed package: ${JSON.stringify(result)}`);
+  });
+
+program
+  .command('updatePackage <environment> <name>')
+  .option('--json', 'Output as JSON')
+  .action(async (environment, name, cmd) => {
+    const result = await manager.updatePackage(environment, name, cmd.json);
+    logger.info(`Updated package: ${JSON.stringify(result)}`);
+  });
+
+
+  program
+  .command('searchForPackage <name> [args]')
+  .allowUnknownOption()
+  .option('--json', 'Output as JSON')
+  .action(async (name, cmd) => {
+    const args = cmd.args.slice(1);
+    const result = await manager.searchForPackage(name, cmd.json, args);
+    logger.info(`Search Results: ${JSON.stringify(result)}`);
+  });
+
+program
+  .command('exportEnvironment <name>')
+  .option('--json', 'Output as JSON')
+  .action(async (name, cmd) => {
+    const result = await manager.exportEnvironment(name, cmd.json);
+    logger.info(`Environment export: ${JSON.stringify(result)}`);
+  });
+
+program
+  .command('getCondaVersion')
+  .option('--json', 'Output as JSON')
+  .action(async (cmd) => {
+    const version = await manager.getCondaVersion(cmd.json);
+    logger.info(`Conda version: ${JSON.stringify(version)}`);
+  });
+
+program
+  .command('condaInfo [args]')
+  .allowUnknownOption()
+  .option('--json', 'Output as JSON')
+  .action(async (cmd) => {
+    const args = cmd.args;
+    const info = await manager.condaInfo(cmd.json, args);
+    logger.info(`Conda Info: ${JSON.stringify(info)}`);
+  });
+
+program
+  .command('runCommand <environment> <command>')
   .action(async (environment, command) => {
-    console.log(`Running command '${command}' in environment ${environment}...`);
     try {
-      const result = await environmentManager.runCommand(environment, command);
-      console.log(`Command result:`, result);
+      const result = await manager.runCommand(environment, command);
+      logger.info(`Command Output: ${result}`);
     } catch (error) {
-      console.error(`Failed to run command '${command}' in environment ${environment}:`, error);
+      logger.error(`Failed to run command: ${error}`);
     }
   });
 
-program.parse(process.argv);
+  program.command('*', { noHelp: true }).action(() => {
+    program.outputHelp();
+  });
+
+  program.parse(process.argv);
